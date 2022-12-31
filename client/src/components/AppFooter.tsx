d43df942773ad096745082443cababb815b30671
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2022-12-31 17:22:50 +1100

    "Made by.." in footer is now visible in light mode

--- client/src/components/AppFooter.tsx
+++ client/src/components/AppFooter.tsx
@@ -4,7 +4,7 @@ import { BrandGithub } from "tabler-icons-react";
 export default function() {
   return (
     <Footer height={40} p='xs' sx={(theme) => ({
-      color: theme.colors.blue[1]
+      color: theme.colorScheme === 'dark' ? theme.colors.blue[1] : theme.colors.blue[9]
     })}>
       <Group align='center'>
         <Text>Made by Omniscient</Text>
